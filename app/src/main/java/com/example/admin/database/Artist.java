818ffc49085273ca145cc5747524bd45d191f2bf
ADMIN <aidarahayu3195@gmail.com>
2017-11-02 19:20:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/admin/database/Artist.java
@@ -0,0 +1,29 @@
+package com.example.admin.database;
+
+/**
+ * Created by ADMIN on 11/2/2017.
+ */
+
+public class Artist {
+
+    String artistId;
+    String artistName;
+
+    public Artist(){}
+
+    public Artist(String artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+
+
+}
